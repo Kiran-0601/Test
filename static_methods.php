57c47e267d0f0fdf735b83e50a99b9e9975c2a43
Kiran-0601 <kiranramchandani26@gmail.com>
2023-05-30 15:47:01 +0530

    Implemented static methods & properties

--- static_methods.php
+++ static_methods.php
@@ -1,19 +1,22 @@
 <?php
 class classname{
-  public static function method(){
+  public static $value = "Hello";    // Defined static property
+  public static function method(){   // Defined static method
     echo "Good Morning";
   }
   public function method2(){
+    echo self::$value;  // called static property
     self::method(); // called static methods
   }
 }
 class subclass extends classname{
     public function method3()
     {
-        echo parent::method();   //To call a static method from a child class
+        echo parent::$value;     // called static property from child class
+        echo parent::method();   // To call a static method from a child class
     }
 }
-
+echo classname::$value;  // call static property
 classname::method(); // call static method using class name without creating object.
 
 $name = new classname();
