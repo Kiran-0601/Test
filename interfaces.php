54f8403cd433575cbb43a2e1f7a6fffce6c4b92b
Kiran-0601 <kiranramchandani26@gmail.com>
2023-05-30 11:59:12 +0530

    Implemented Interfaces

---
+++ interfaces.php
@@ -0,0 +1,38 @@
+<?php
+interface Father {
+  public function bussiness();
+}
+
+interface Mother {
+    public function job();
+}
+
+interface Child extends Father, Mother{    // Multiple Inheritance by using more interfaces
+    public function study();
+}
+
+class Boy {
+    public function play() {
+        echo "My brother is playing.";
+    }
+}
+
+class Girl extends Boy implements Child {  // Accessed Multiple Inheritance by using Interface..
+
+  public function bussiness() {
+    echo "My father is doing bussiness.";
+  }
+  public function job() {
+    echo "My mother is doing job.";
+  }
+  public function study() {
+    echo "I m doing study.";
+  }
+}
+
+$child = new Girl();
+$child->bussiness();
+$child->job();
+$child->study();
+$child->play();
+?> 
