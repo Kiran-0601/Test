259327a6f3dfe49b38f2f693ac255ce32525a348
Kiran-0601 <kiranramchandani26@gmail.com>
2023-06-16 18:54:23 +0530

    Implemented Authentication Module

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'ramchandanik872@gmail.com';
-$mail->Password = 'ghaxkaaphnmbwndb';
+$mail->Password = ' ';       // Secrete Key 
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
  
