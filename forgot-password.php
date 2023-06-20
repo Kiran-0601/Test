768d844c3ae73f56246f29e326de43f7f12db57d
Kiran-0601 <kiranramchandani26@gmail.com>
2023-06-20 14:09:44 +0530

    Implemted forgot-password

--- forgot-password.php
+++ forgot-password.php
@@ -22,36 +22,35 @@ session_start();
     max-width: 450px;
   }
   </style>
-  <title>SignIn Form</title>
+  <title>Forgot Password Form</title>
 </head>
 <body>
 <?php
   require 'db.php';
+  require 'mail.php';
 
   if (isset($_POST['submit'])){
-   
+    
     $email = $_POST['email'];
-    $pwd =  $_POST['pwd'];
     $sql = "SELECT * FROM auth WHERE email LIKE '$email'";
     $result = $conn->query($sql);
     // Check user exist or not.
     if ($result->num_rows > 0) {
-      $row = $result->fetch_assoc();
-      $storedPassword = $row["password"];
-      $id = $row["id"];
-      // Verify the password
-      if ($pwd == $storedPassword) {
-        // Authentication successful, redirect to dashboard or homepage
-        $_SESSION['email'] = $email;
-        $_SESSION['id'] = $id;
-        header("Location: dashboard.php");
-      } else {
-        // Invalid password, show an error message
-        echo "Invalid Your password.";
-      }
+        // send email 
+        $token = bin2hex(random_bytes(32));
+        $mail->isHTML(true);
+        $mail->setFrom('ramchandanik872@gmail.com', 'Concetto Labs');
+        $mail->addAddress($email);
+        $mail->Subject = "Password Reset Mail";
+        $mail->Body = '<h1>Reset Your Password</h1>
+        <p>Please click the following link to reset your password:</p>
+        <a href="http://localhost/php/Authentication/reset-password.php?token=' . $token . '">Reset Password</a>';
+        if ($mail->send()) {
+            echo "<h5>Registration Successfully !! Plz check Your Email..</h5>";
+        }
     } else {
-      // Invalid username, show an error message
-      echo "Invalid Your Username";
+        // Invalid username, show an error message
+        echo "Invalid Your Email";
     }
     // Close the database connection
     $conn->close();
@@ -59,20 +58,15 @@ session_start();
 ?>
     
 <div class="wrapper">
-  <form id="signin" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" class="form-right">
-    <h2 class="text-center">SignIn Form</h2>
+  <form id="forgotpassword" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" class="form-right">
+    <h2 class="text-center">Forgot Password</h2>
     <div class="mb-3">
       <label for="email">Email address</label>
       <input type="email" name="email" class="input-field" id="email">
     </div>
-    <div class="mb-3">
-      <label for="password">Password</label>
-      <input type="password" name="pwd" class="input-field" id="pwd">
-    </div>
     <div class="form-field">
       <input type="submit" value="Login" class="register" name="submit">
-      <p style="color: #800000;">New Register ?? <a href="signup.php">Please SignUp</a></p>
-      <p style="color: #800000;">Forgot Password ?? <a href="forgot-password.php">Click here</a></p>
+      <p><a href="signin.php" style="color: #800000;">Back To Login</a></p>
     </div>
   </form>
 </div>
